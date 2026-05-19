e27927b0abee3d92241da78d78580af66eb1615f
Zach Warres <zwarres@gmail.com>
2026-05-19 12:54:03 -0700

    Added yocto install script and updated rust install script for Yocto build

--- linux-project/comms-project/install_executable.sh
+++ linux-project/comms-project/install_executable.sh
@@ -6,9 +6,9 @@ set -e
 # --- Configuration ---
 BINARY_NAME="comms-project"
 TARGET_ARCH="arm-unknown-linux-gnueabihf"
-TARGET_HOST="zachs-pi-zero.local"
-TARGET_USER="zwarres"
-TARGET_DIR="/home/zwarres/Documents/Projects/AnemometerComms"
+TARGET_HOST="raspberrypi0-wifi.local"
+TARGET_USER="root"
+TARGET_DIR="/usr/bin"
 
 # The local path where Cargo outputs the binary
 LOCAL_BIN_PATH="./target/${TARGET_ARCH}/release/${BINARY_NAME}"
@@ -19,6 +19,6 @@ echo "🔨 Building ${BINARY_NAME} for ${TARGET_ARCH}..."
 cross build --target ${TARGET_ARCH} --release
 
 echo "🚀 Deploying to ${TARGET_HOST}..."
-scp ${LOCAL_BIN_PATH} ${TARGET_USER}@${TARGET_HOST}:${TARGET_DIR}
+scp -O ${LOCAL_BIN_PATH} ${TARGET_USER}@${TARGET_HOST}:${TARGET_DIR}
 
 echo "✅ Done! Binary is at: ${TARGET_DIR}/${BINARY_NAME}"
